e6816b351aeaeb2a630006016fbe1566cc7a3426
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-10-15 00:53:35 +0530

    bdb

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", 
+        target: "https://badhaibazaarbackend.onrender.com", 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), 
       },
